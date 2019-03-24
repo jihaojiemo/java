d694b538dad1e9e78f5f75727675e3a7ce704afd
jihaojiemo <914064689@qq.com>
2019-03-24 17:38:38 +0800

    overload

---
+++ FangFaChongZai.java
@@ -0,0 +1,12 @@
+public class FangFaChongZai {
+    public static void main(String[] args) {
+      System.out.println(add(10, 20));
+      System.out.println(add(30, 40));
+    }
+    public static int add (int x, int y) {
+        return x + y;
+        }
+    public static double add (int x, int y) {
+        return x + y;
+        } 
+}
