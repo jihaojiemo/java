10bbfb2fd2c0d65968b39c7824f4df26f4bc5a4e
jihaojiemo <914064689@qq.com>
2019-05-25 23:33:46 +0800

    带头循环单链表

--- 单链表/src/com/bittech/CircleLinked/CHeadSingleListImpl.java
+++ 单链表/src/com/bittech/CircleLinked/CHeadSingleListImpl.java
@@ -1,4 +1,162 @@
 package com.bittech.CircleLinked;
 
-public class CHeadSingleListImpl {
+public class CHeadSingleListImpl implements ICLinked {
+
+    class Node {
+        private int data;
+        private Node next;
+
+        public Node() {
+            this.data = -1;
+            this.next = null;
+        }
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    public CHeadSingleListImpl() {
+        this.head = new Node();
+        this.head.next = this.head;
+    }
+
+    @Override
+    public void addFirst(int data) {
+        Node node = new Node(data);
+        node.next = this.head.next;
+        this.head.next = node;
+    }
+
+    @Override
+    public void addLast(int data) {
+        Node node = new Node(data);
+        Node cur = this.head;
+        while (cur.next != this.head) {
+            cur = cur.next;
+        }
+
+        node.next = cur.next;
+        cur.next = node;
+    }
+
+    @Override
+    public boolean addIndex(int index, int data) {
+
+        Node cur = this.head;
+
+        if(index < 0 || index > getLength()) {
+            return false;
+        }
+
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+
+        //cur就是index位置的前驱
+        Node node = new Node(data);
+        node.next = cur.next;
+        cur.next = node;
+
+        return true;
+    }
+
+    @Override
+    public boolean contains(int key) {
+
+        Node cur = this.head.next;
+        while (cur != this.head) {
+            if (cur.data != key) {
+                cur = cur.next;
+            }
+            return true;
+        }
+        return false;
+    }
+
+
+    private Node searchPre(int key) {
+
+        Node pre = this.head;
+        while (pre.next != this.head) {
+            if (pre.next.data != key) {
+                pre = pre.next;
+            }
+            return pre;
+        }
+        return null;
+    }
+
+
+    @Override
+    public int remove(int key) {
+
+        Node pre = searchPre(key);
+        int oldData = 0;
+
+        if(pre == null) {
+            throw new Error();
+        }
+
+        oldData = pre.next.data;
+        pre.next = pre.next.next;
+
+        return oldData;
+    }
+
+    @Override
+    public void removeAllKey(int key) {
+
+        if (this.head == null || this.head.next == this.head) {
+            return;
+        }
+        Node pre = this.head;
+        Node cur = this.head.next;
+
+        while (cur != this.head) {
+            if (cur.data == key) {
+                pre.next = cur.next;
+                cur = cur.next;
+            }else {
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+    }
+
+    @Override
+    public int getLength() {
+
+        //头结点不算
+        Node cur = this.head.next;
+        int count = 0;
+        while (cur != this.head) {
+            count++;
+            cur = cur.next;
+        }
+        return count;
+    }
+
+    @Override
+    public void display() {
+
+        Node cur = this.head.next;
+        while (cur != this.head) {
+            System.out.print(cur.data + " ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void clear() {
+
+        while (this.head.next != this.head) {
+            Node cur = this.head.next;
+            this.head.next = cur.next;
+        }
+        this.head = null;
+    }
 }
--- 单链表/src/com/bittech/CircleLinked/Test.java
+++ 单链表/src/com/bittech/CircleLinked/Test.java
@@ -6,4 +6,45 @@ package com.bittech.CircleLinked;
  * Date: 2019-05-23 11:39:19
  */
 public class Test {
+
+    public static void main(String[] args) {
+
+        CHeadSingleListImpl list = new CHeadSingleListImpl();
+
+        list.addFirst(10);
+        list.addLast(20);
+        list.addFirst(30);
+        list.addFirst(40);
+        list.addLast(50);
+        list.addLast(10);
+
+        System.out.print("原链表：");
+        list.display(); //40 30 10 20 50 10
+
+        System.out.println("链表的长度：" + list.getLength());
+
+        list.addIndex(3, 88);
+        System.out.print("添加后的链表：");
+        list.display();//40 30 10 88 20 50 10
+
+        System.out.println("链表是否包含元素88：" + list.contains(88));
+        System.out.println("链表是否包含元素100：" + list.contains(100));
+
+        list.remove(88);
+        System.out.print("删除88之后的链表：");
+        list.display();
+
+        list.removeAllKey(10);
+        System.out.print("删除所有值为10的节点后的链表：");
+        list.display();
+
+        list.clear();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.print("清空后的单链表：");
+        list.display();
+    }
 }
