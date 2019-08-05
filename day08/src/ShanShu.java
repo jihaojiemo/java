8e263d26c98d103280d2f9d56e58fed864a96cf5
jihaojiemo <914064689@qq.com>
2019-08-05 19:47:01 +0800

    删数

---
+++ day08/src/ShanShu.java
@@ -0,0 +1,28 @@
+/**
+ * Description:
+ * Author: admin
+ * Create: 2019-07-28 17:23
+ */
+import java.util.*;
+public class ShanShu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            if (n > 1000) {
+                n = 999;
+            }
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                list.add(i);
+            }
+            int i = 0;
+            while (list.size() > 1) {
+                i = (i + 2) % list.size();
+                list.remove(i);
+            }
+            System.out.println(list.get(0));
+        }
+    }
+}
+
