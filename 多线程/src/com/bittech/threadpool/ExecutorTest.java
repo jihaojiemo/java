f2ed2928f52c64cfbd2e61f12ee64a72e5dd1082
jihaojiemo <914064689@qq.com>
2019-08-09 17:46:13 +0800

    ExecutorServer

---
+++ 多线程/src/com/bittech/threadpool/ExecutorTest.java
@@ -0,0 +1,16 @@
+package com.bittech.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Description:
+ * Author: admin
+ * Create: 2019-08-04 18:29
+ */
+public class ExecutorTest {
+    public static void main(String[] args) {
+        //创建一个线程池对象
+        ExecutorService executorService = new ThreadPoolExecutor();
+    }
+}
